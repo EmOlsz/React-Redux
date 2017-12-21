37550ac47e488c03b148411995d20439df4733fc
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2017-12-22 00:17:59 +0100

    added video_list and video_list_item components with props

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 // Library
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-// Components
-import SearchBox from "./components/search_bar";
-
+import YTSearch from 'youtube-api-search';
+import SearchBox from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBzCcqKCjEjjlXWhYoVTWx_gSasQ5XgDw8';
+const term = 'Stone temple pilots';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBox />
-        </div>
-    );
-};
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+            videos: []
+        };
+        YTSearch({key: API_KEY, term: term}, (videos) => this.setState({videos}));
+    }
+    render() {
+        return (
+            <div>
+                <SearchBox />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
